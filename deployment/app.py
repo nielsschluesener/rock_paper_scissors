1cc71346e01d12e92a2e1c7326ebf7da8f0d99fc
Niels Schlüsener <nschluesener@live.com>
2021-12-06 10:57:01 +0100

    dplymnt css/html/js update

--- deployment/app.py
+++ deployment/app.py
@@ -6,10 +6,10 @@ import tensorflow as tf
 
 
 app=Flask(__name__)
-camera=cv2.VideoCapture(0)
+y_labels = ['Paper', 'Rock', 'Scissors']
 model = tf.keras.models.load_model(os.path.join(app.root_path, 'static', 'model.h5'))
 
-y_labels = ['Paper', 'Rock', 'Scissors']
+camera=cv2.VideoCapture(0)
 
 def generate_frames():
     while True:
@@ -21,7 +21,7 @@ def generate_frames():
             frame=buffer.tobytes()
 
         yield(b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def index():
@@ -40,8 +40,26 @@ def pred():
         img = cv2.resize(frame, (224, 224)).reshape([1,224,224,3])
         pred_proba = model.predict(img)
         pred_binary = np.argmax(pred_proba, axis=1)
+        pred_label = y_labels[pred_binary[0]]
+        pred_proba = '{:2.2f}%'.format(np.amax(pred_proba)*100)
 
-        return render_template('prediction.html' , probabilities = pred_proba, prediction = pred_binary, display_image = 'input_imgs/' + frame_name)
+        ai_label = y_labels[np.random.randint(0, len(y_labels))]
+        ai_img = '{}.jpg'.format(ai_label)
+
+        if (ai_label == 'Paper' and pred_label == 'Scissors') or (ai_label == 'Rock' and pred_label == 'Paper') or (ai_label == 'Scissors' and pred_label == 'Rock'):
+            result_label = 'You win!'
+        elif (pred_label == 'Paper' and ai_label == 'Scissors') or (pred_label == 'Rock' and ai_label == 'Paper') or (pred_label == 'Scissors' and ai_label == 'Rock'):
+            result_label = 'You lose!'
+        elif (pred_label == ai_label):
+            result_label = 'Draw!'
+
+        return render_template('prediction.html', 
+                                probability = pred_proba, 
+                                prediction = pred_label.upper(), 
+                                display_image = 'input_imgs/' + frame_name,
+                                game_choice = ai_label.upper(),
+                                game_image = 'assets/img/choices/' + ai_img,
+                                game_result = result_label.upper())
 
 @app.route('/video')
 def video():
---
+++ deployment/static/assets/js/script.js
@@ -0,0 +1,65 @@
+var picForm = document.querySelector('form');
+
+picForm.addEventListener('submit', event => {
+    event.preventDefault();
+
+    console.log('shutter_triggered');
+
+    var timer = document.getElementById('shutter_btn');
+    var title = document.getElementById('title');
+    var explanation = document.getElementById('explanation');
+
+    timer.setAttribute('disabled', 'disabled');
+    timer.style.fontSize = "2.2rem";
+    timer.innerHTML = 5;
+
+    title.style.color = '#fff';
+    explanation.style.color = '#fff';
+
+    document.body.style.backgroundColor = '#1BD2B2';
+
+
+
+    var countdown = window.setInterval(function () {
+
+
+
+        var seconds = timer.innerHTML;
+        seconds = seconds - 1;
+        timer.innerHTML = seconds;
+
+        if (seconds == 0) {
+
+            document.body.style.transition = "all .2s";
+            document.body.style.backgroundColor = '#fff';
+
+            timer.innerHTML = 'Cheese!';
+
+            clearInterval(countdown);
+        }
+
+    }, 1000);
+
+    window.setTimeout(function () {
+
+        console.log('play_sound');
+
+        var audio = new Audio('assets/sfx/shutter_sfx.wav');
+        audio.play();
+
+        window.setTimeout(function () {
+
+            document.body.style.backgroundColor = '#1BD2B2';
+
+            window.setTimeout(function () {
+
+                picForm.submit();
+                console.log('form_submitted');
+
+            }, 600);
+
+        }, 350);
+
+    }, 5000);
+
+});
